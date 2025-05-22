d250241333c58a308e4602a8a1e21f6959584b85
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-05-22 16:05:33 -0700

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -47,7 +47,41 @@ class experiment:
         return p_value
 
     def two_sample_t_test(self,claim,alpha,tails=2,print=False):
-        
-        
-                
+        """
+        Perform 2 sample t test and confidence interval
+        """
+        if self._dependence == True:
+            return None
+        if len(self._treatments) != 2:
+            return None
+        # first get standard error
+        std_error = (self._samples[0].get_standard_deviation()/self._samples[0].get_count()+self._samples[1].get_standard_deviation()/self._samples[1].get_count())**.5
+        # get t test stat
+        diff = self._samples[0].get_mean()-self._samples[1].get_mean()
+        t_stat = (diff-claim)/std_error
+        deg_of_freedom = self.satterthwaite(self._samples[0].get_standard_deviation(),self._samples[1].get_standard_deviation(),self._samples[0].get_count(),self._samples[1].get_count())
+        margin_of_error = st.t.ppf(alpha/2,deg_of_freedom) * std_error
+        lower_bound = diff - margin_of_error
+        upper_bound = diff + margin_of_error
+        p_value = (1-st.t.cdf(t_stat,deg_of_freedom))*tails
+        if print:
+            print(f"The probability that the 2 samples have a difference of {diff}, when the proposed difference is {claim}, is {p_value}, for a {tails} tailed t-test.")
+            if p_value < alpha:
+                print(f"The null hypothesis of {claim} is rejected at a significance level of {alpha}, from a t-stat of {t-stat}")
+            print(f"The {(1-alpha)*100}% confidence interval for the mean is from {low_bound} to {upper_bound} with a margin of error of {margin_of_error}")
+        return p_value
+
+    def satterthwaite(self,s1,s2,n1,n2):
+        num = (s1**2/n1+s2**2/n2)**2
+        denom = ((s1**2/n1)**2/(n1-1))+((s2**2/n2)**2/(n2-1))
+        return floor(num/denom)
             
+    def single_factor_ANOVA(self):
+        """
+        Perform a single factor ANOVA analysis
+        """
+        # Get MSTr
+        # Get MSE
+        # Get SST
+        # Get SSTR
+        # Get SSE
