c5e1d9257f4eb104acd0e3072cd04c77dd31f2bd
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-05-23 10:47:44 -0700

    Update models.py

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class ANOVA:
         self.find_SSTr()
         self.find_SST()
         
-    def find_grand_mean():
+    def find_grand_mean(self):
         tot = 0
         data_points = 0
         for i in range(self._I):
@@ -24,8 +24,28 @@ class ANOVA:
             tot += tmp
         self._grand_mean = tot/data_points
         
-    def find_MSTr():
+    def find_MSTr(self):
+        """
+        Find mean squared treatment
+        """
+        # Presently only works for J being equal amongst samples
         square_error = 0
         for i in range(self._I):
             square_error += (self._responses[i].get_mean()-self._grand_mean)**2
         square_error *= self._J
+        square_error /= (self._I-1)
+        self._MSTr = square_error
+
+    def find_MSE(self):
+        square_error = 0
+        for i in range(self._I):
+            square_error += self._responses[i].get_standard_deviation()**2
+        square_error /= self._I
+        self._MSE = square_error
+
+    def find_SST_quants(self):
+        for i in range(self._I):
+            var = 0
+            for j in range(self._responses[i].get_count()):
+                var += (self._responses[i][j] - self._grand_mean)**2
+        self._SST = var
